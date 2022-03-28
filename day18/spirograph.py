ce29b1d45944bd726f4daf52ac195aca864ed265
Sam Marston <sammarston15@gmail.com>
2022-03-27 17:10:24 -0700

    fix: spirograph now stops after 360 degrees

--- day18/spirograph.py
+++ day18/spirograph.py
@@ -11,17 +11,20 @@ def random_color():
   r = random.randint(0, 255)
   g = random.randint(0, 255)
   b = random.randint(0, 255)
-  random_color = (r, g, b)
-  return random_color
+  color = (r, g, b)
+  return color
 
 
 
 # tim.pensize(10)
+def draw_spirograph(size_of_gap):
+  for i in range(int(360 / size_of_gap)):
+    tim.color(random_color())
+    tim.circle(100)
+    tim.setheading(tim.heading() + size_of_gap)
 
-for i in range(150):
-  tim.circle(100)
-  tim.color(random_color())
-  tim.left(10)
+
+draw_spirograph(5)
 
 screen = Screen()
 screen.exitonclick()
