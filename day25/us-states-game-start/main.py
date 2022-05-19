710495e96f04ce3c6c8365e6ed4478cd4b37618d
sammarston15 <sammarston15@gmail.com>
2022-05-19 07:32:20 -0700

    finished states game and day25

--- day25/us-states-game-start/main.py
+++ day25/us-states-game-start/main.py
@@ -1,3 +1,4 @@
+from sys import breakpointhook
 import turtle
 import pandas
 
@@ -7,13 +8,23 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
 guessed_states = []
 
-while guessed_states < 50:
-    data = pandas.read_csv("50_states.csv")
+while len(guessed_states) < 50:
     all_states = data.state.to_list()
 
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        print('exit')
+        break
 
     if answer_state in all_states:
         guessed_states.append(answer_state)
@@ -23,17 +34,3 @@ while guessed_states < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(state_data.state.item())
-
-
-
-def ask_user():
-    print(f"user's guess: {answer_state}")
-
-
-
-
-
-
-
-
-turtle.mainloop()
