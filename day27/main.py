ced0c3ffd34632381420ae0394e4992d76d603b5
Sam Marston <smarston@easypost.com>
2022-06-02 07:59:43 -0700

    finished day27

--- day27/main.py
+++ day27/main.py
@@ -1,17 +1,31 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
 # create label in window
-label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 # the packer is what allows the label to show on the screen in the window
-label.pack(side="left")
+label.grid(column=0, row=0)
+label.config(padx=50, pady=50)
 
+# Button
+def button_clicked():
+    print("I got clicked")
+    new_input = input.get()
+    label.config(text=new_input)
 
+button = Button(text="Click me", command=button_clicked)
+button.grid(column=1, row=1)
 
+new_button = Button(text="New Button")
+new_button.grid(column=3, row=0)
 
+# Entry
+input = Entry(width=10)
+input.grid(column=4, row=3)
 
 
 
