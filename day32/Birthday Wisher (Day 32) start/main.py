653c5f2ed1aef7d483c75d8464dc6c3c46ff03c4
Sam Marston <smarston@easypost.com>
2022-06-25 08:16:43 -0700

    a little more work on day32

--- day32/Birthday Wisher (Day 32) start/main.py
+++ day32/Birthday Wisher (Day 32) start/main.py
@@ -15,14 +15,40 @@
 
 
 
+# import datetime as dt
+
+# now = dt.datetime.now()
+# year = now.year
+# if year == 2020:
+#     print("wear a mask")
+# day_of_the_week = now.weekday()
+# print(day_of_the_week)
+
+# date_of_birth = dt.datetime(year=1997, month=11, day=8, hour=4)
+# print(date_of_birth)
+
+
+
+import smtplib
 import datetime as dt
+import random
+
+MY_EMAIL = "email@email.com"
+MY_PASSWORD = "1234567asdf"
 
 now = dt.datetime.now()
-year = now.year
-if year == 2020:
-    print("wear a mask")
-day_of_the_week = now.weekday()
-print(day_of_the_week)
+weekday = now.weekday()
+if weekday == 6:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
 
-date_of_birth = dt.datetime(year=1997, month=11, day=8, hour=4)
-print(date_of_birth)
+    print(quote)
+    with smtplib.SMTP("stmp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL, 
+            to_addrs=MY_EMAIL, 
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
