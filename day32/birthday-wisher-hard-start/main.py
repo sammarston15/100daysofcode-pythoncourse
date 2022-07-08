ed32fbcbb58a2645ccb3bbcd03bed2fa21ef2c2f
Sam Marston <smarston@easypost.com>
2022-07-08 07:44:08 -0700

    finished day32

---
+++ day32/birthday-wisher-hard-start/main.py
@@ -0,0 +1,28 @@
+##################### Hard Starting Project ######################
+
+import datetime as dt
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "email@email.com"
+MY_PASSWORD = "12345abcd"
+
+today_tuple = (dt.datetime.now().month, dt.datetime.now().day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_template/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("{NAME}", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=birthday_person["email"], msg=f"Subject:Happy birthday\n\n{contents}")
+
+
